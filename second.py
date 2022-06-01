2cc142520c5214b110f513d6783fccdfbd232b23
Dragan Golic <golicdragan0@gmail.com>
2022-06-01 15:18:16 -0400

    add a comment

--- second.py
+++ second.py
@@ -1,4 +1,8 @@
-class A ():
-    def __init__(self,x,y):
-        self.x = x
-        print("Jo")
+for row in range(7):
+    for col in range(8):
+        if (row+col==7) or (row==0 and col==1) or (row==1 and col==2) or (row==2 and col==3):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+
