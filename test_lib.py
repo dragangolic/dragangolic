1d5502af09b51be5f320c7b48ab97f12b1df5385
dragangolic <105243599+dragangolic@users.noreply.github.com>
2022-06-02 15:54:04 -0400

    Update test_lib.py

--- test_lib.py
+++ test_lib.py
@@ -11,3 +11,22 @@ while guess_count<guess_limit:
 else:
     print("Sorry,more luck next time.")
 # just the comment
+
+class School():
+    def __init__(self):
+        self.__goingtoschool()
+    def learning(self):
+        print("OOP and encapsulation")
+    def study(self):
+        print("Python")
+    def __goingtoschool(self):
+        print("I love coding")
+laptop = School()
+laptop.learning()
+laptop1 = School()
+laptop1.study()
+class addingSchool(School):
+    def __init__(self):
+        School.__init__(self)
+        self.__sum = 0
+
