ed546b53913a8bbd4b8a8def078411e5c96310e1
Dragan Golic <golicdragan0@gmail.com>
2022-06-03 12:36:50 -0400

    Stanko ns

--- second.py
+++ second.py
@@ -1,8 +1,12 @@
-for row in range(7):
-    for col in range(8):
-        if (row+col==7) or (row==0 and col==1) or (row==1 and col==2) or (row==2 and col==3):
-            print("*",end="")
-        else:
-            print(" ",end="")
-    print()
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
 
+def about_me(name, profession, pet, hobby):
+    print(Fore.RED + "Hi, I am", name)
+    print(Back.GREEN + "I am a", profession)
+    print(Fore.MAGENTA + "And I have ", pet)
+    print(f"{Back.CYAN} I love to play", hobby)
+about_me("Dragan", "programmer", "bees", "guitar")
+print(f"{Fore.BLUE } I hope you are going to like my small repo projects")
+print(f"{Style.NORMAL}{Fore.LIGHTBLUE_EX}{Back.YELLOW} Have a lovely day")
