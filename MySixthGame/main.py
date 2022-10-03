44e3e9929f1f9be07739d36a2e33685840ecabeb
Dragan Golic <golicdragan0@gmail.com>
2022-10-03 12:19:25 -0400

    nice game

--- MySixthGame/main.py
+++ MySixthGame/main.py
@@ -1,22 +1,24 @@
 import random
 from words import words
 import string
-
-user_input = input('Type something')
+from hangman_visual import lives_visual_dict
 
 
 def get_valid_word(words):
     word = random.choice(words)
-    return word
+    while '-' in word or ' ' in word:
+        word = random.chice(words)
+
+    return word.upper()
 
 
 def hangman():
     word = get_valid_word(words)
     word_letters = set(word)  # letters in word
-    alphabet = set(str(string.ascii_uppercase))
+    alphabet = set(string.ascii_uppercase)
     used_letters = set()  # what the user has guessed
 
-    lives = 6
+    lives = 7
 
     # getting user input
     while len(word_letters) > 0 and lives > 0:
@@ -26,6 +28,7 @@ def hangman():
 
         # what current word is ( ie W - RD)
         word_list = [letter if letter in used_letters else '-' for letter in word]
+        print(lives_visual_dict[lives])
         print('Current word: ', ' '.join(word_list))
 
         user_letter = input('Guess a letter: ').upper()
@@ -33,25 +36,28 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+                print('')
 
             else:
                 lives = lives - 1
-                print('Letter is found in word.')
+                print('\nYour letter,', user_letter, 'is not in the word')
 
         elif user_letter in used_letters:
-            print('You have already used that character. Please try again.')
+            print('\nYou have already used that letter. Try again.')
 
         else:
-            print('Invalid character. Please try again')
+            print('\nInvalid character. Please, try again')
 
     # gets here when len(word_letters) == 0 OR when lives == 0
     if lives == 0:
+        print(lives_visual_dict[lives])
         print('You lost, sorry. The word was', word)
     else:
         print('You won', word, 'is correct!')
 
 
-hangman()
+if __name__ == '__main__':
+    hangman()
 
 
 
